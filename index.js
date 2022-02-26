abbbf43d5cb5cb8f4278c3495f4b861bc7476efb
Kim Green <kimgreen.writings@gmail.com>
2022-02-26 17:01:01 +0200

    commit tree 54f59f5544bd1d82f395b668566c37357dc0c616

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+// Write a function called superbowlWin() in index.js:
+
+
+const superbowlWin = (records) => {
+    const win = records.find(record => record.result === "W")
+    return win ? win.year : undefined
+} 
+
+// if (win) {
+//     return win.year
+// } else {
+//     return undefined
+// }
+
+superbowlWin(record);
+
+// The function should receive 1 argument, an Array of JavaScript Objects
+// Each object has two properties: year and result
+// It should use find() to test each Object to see if the result is "W" — a win!
+// It should return the year when the win occurred (if it occurred at all!)
+// If no win is found, it should return, sadly, undefined
